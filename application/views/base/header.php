ec2b5370e88a0a9a4510ee5fb3a9ac781f521e47
jordanzet <jordanzet@gmail.com>
2017-04-06 14:54:17 -0500

    arreglos pagina contacto header y footer

--- application/views/base/header.php
+++ application/views/base/header.php
@@ -84,99 +84,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<div id="header-top-second"  class="clearfix">
 								<div class="header-top-dropdown">
 									<div class="btn-group dropdown">
-										<button type="button" class="btn dropdown-toggle"><a href="mailto:info@emprendehub.org">info@emprendehub.org</a></button>
+										<button type="button" class="btn dropdown-toggle"><a href="mailto:info@emprendehub.org"><i class="fa fa-envelope"></i> info@emprendehub.org</a></button>
 									</div>
 									<div class="btn-group dropdown">
-										<button type="button" class="btn dropdown-toggle">(+51) 967 678 152</button>
-									</div>
-
-									<div class="btn-group dropdown">
-										<button type="button" class="btn dropdown-toggle" data-toggle="dropdown"><i class="fa fa-search"></i> Search</button>
-										<ul class="dropdown-menu dropdown-menu-right dropdown-animation">
-											<li>
-												<form role="search" class="search-box">
-													<div class="form-group has-feedback">
-														<input type="text" class="form-control" placeholder="Search">
-														<i class="fa fa-search form-control-feedback"></i>
-													</div>
-												</form>
-											</li>
-										</ul>
-									</div>
-									<div class="btn-group dropdown">
-										<button type="button" class="btn dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Login</button>
-										<ul class="dropdown-menu dropdown-menu-right dropdown-animation">
-											<li>
-												<form class="login-form">
-													<div class="form-group has-feedback">
-														<label class="control-label">Username</label>
-														<input type="text" class="form-control" placeholder="">
-														<i class="fa fa-user form-control-feedback"></i>
-													</div>
-													<div class="form-group has-feedback">
-														<label class="control-label">Password</label>
-														<input type="password" class="form-control" placeholder="">
-														<i class="fa fa-lock form-control-feedback"></i>
-													</div>
-													<button type="submit" class="btn btn-group btn-dark btn-sm">Log In</button>
-													<span>or</span>
-													<button type="submit" class="btn btn-group btn-default btn-sm">Sign Up</button>
-
-													<ul>
-														<li><a href="#">Forgot your password?</a></li>
-													</ul>
-													<div class="divider"></div>
-													<span class="text-center">Login with</span>
-													<ul class="social-links clearfix">
-														<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
-														<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-														<li class="googleplus"><a target="_blank" href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
-													</ul>
-												</form>
-											</li>
-										</ul>
-									</div>
-									<div class="btn-group dropdown">
-										<button type="button" class="btn dropdown-toggle" data-toggle="dropdown"><i class="fa fa-shopping-cart"></i> Cart (8)</button>
-										<ul class="dropdown-menu dropdown-menu-right dropdown-animation cart">
-											<li>
-												<table class="table table-hover">
-													<thead>
-														<tr>
-															<th class="quantity">QTY</th>
-															<th class="product">Product</th>
-															<th class="amount">Subtotal</th>
-														</tr>
-													</thead>
-													<tbody>
-														<tr>
-															<td class="quantity">2 x</td>
-															<td class="product"><a href="shop-product.html">Android 4.4 Smartphone</a><span class="small">4.7" Dual Core 1GB</span></td>
-															<td class="amount">$199.00</td>
-														</tr>
-														<tr>
-															<td class="quantity">3 x</td>
-															<td class="product"><a href="shop-product.html">Android 4.2 Tablet</a><span class="small">7.3" Quad Core 2GB</span></td>
-															<td class="amount">$299.00</td>
-														</tr>
-														<tr>
-															<td class="quantity">3 x</td>
-															<td class="product"><a href="shop-product.html">Desktop PC</a><span class="small">Quad Core 3.2MHz, 8GB RAM, 1TB Hard Disk</span></td>
-															<td class="amount">$1499.00</td>
-														</tr>
-														<tr>
-															<td class="total-quantity" colspan="2">Total 8 Items</td>
-															<td class="total-amount">$1997.00</td>
-														</tr>
-													</tbody>
-												</table>
-												<div class="panel-body text-right">
-												<a href="shop-cart.html" class="btn btn-group btn-default btn-sm">View Cart</a>
-												<a href="shop-checkout.html" class="btn btn-group btn-default btn-sm">Checkout</a>
-												</div>
-											</li>
-										</ul>
-									</div>
+										<button type="button" class="btn dropdown-toggle"><i class="fa fa-phone"></i> (+51) 967 678 152</button>
+									</div>								
 								</div>
 							</div>
 						</div>
